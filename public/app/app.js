a23bcf93765b8ae91e3d6600040e47ac1c04d319
Adler Coelho Santos <adlersantos@ftd.com.br>
2018-03-15 13:28:59 -0300

    Adding new modules and utils functions.

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,10 @@
-document.querySelector('#myButton').onclick = () => alert('hello world');
+import { handleStatus, log } from './utils/promise-helpers.js';
+
+document.querySelector('#myButton').onclick = () => 
+    fetch('http:///localhost:3000/notas')
+    .then(handleStatus)
+    .then(notas => notas.$flatMap(nota => notas.itens))
+    .then(itens => itens.filter(item => item.codigo == '2143'))
+    .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+    .then(total => log(total))
+    .catch(err => console.error('Aconteceu um erro:', err))
