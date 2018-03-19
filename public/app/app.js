05f3dfafe2c65f6608f2273be70a7c80b41d32c2
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2018-03-18 22:10:29 -0300

    creating a takeUntil.

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,18 @@
 import { log } from './utils/promise-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil } from './utils/operators.js';
 import './utils/array-helpers.js';
 
-document.querySelector('#myButton').onclick = () => 
+const operation = takeUntil(3, () =>
     service
-    .sumItems('2143')
-    .then(total => console.log('total', total))
-    .catch(err => console.error('Aconteceu um erro:', err))
+        .sumItems('2143')
+        .then(total => console.log('total', total))
+        .catch(err => console.error('Aconteceu um erro:', err))
+);
+
+document
+    .querySelector('#myButton')
+    .onclick = () => operation();
 
 // const fn = sumItems('2143')
 // fn(notas);
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -3,3 +3,6 @@ export const partialize = (fn, ...args) => fn.bind(null, ...args);
 export const compose = (...fns) => value =>
     fns.reduceRight((prev, fn) => 
         fn(prev), value);
+
+export const takeUntil = (times, fn) => 
+    () => times-- > 0 && fn();
